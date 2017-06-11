a447d03fb2c806679ce5671a031d786cbdcd69c5
Andi Palo <sanandrea8080@gmail.com>
2017-06-11 15:04:42 +0200

    testing events

--- app/src/main/java/com/example/andi/testdroidapp/MainActivity.java
+++ app/src/main/java/com/example/andi/testdroidapp/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.andi.testdroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.example.awesomelibrary.andi.Events.PlayerEvent;
+import com.example.awesomelibrary.andi.Events.PlayerEventListener;
+import com.example.awesomelibrary.andi.Events.impl.MediaLoadEvent;
+import com.example.awesomelibrary.andi.SimplePlayer;
 
 public class MainActivity extends AppCompatActivity {
     private TextView tv;
@@ -10,9 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.test_label);
-
+        SimplePlayer player = new SimplePlayer((ViewGroup) findViewById(android.R.id.content));
+        player.asEventDispatcher().addEventListener(MediaLoadEvent.getAssociatedType(), new PlayerEventListener<MediaLoadEvent>() {
+            @Override
+            public void onPlayerEvent(MediaLoadEvent param) {
+                tv.setText("Event: "+param);
+            }
+        });
     }
 }
